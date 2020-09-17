fd440b1b07c305d5c961ac96cdab12f0f2f68cfa
jatheesan <you@example.com>
2020-09-18 04:51:54 +0530

    modify the file on master

---
+++ CscExample.java
@@ -0,0 +1,45 @@
+public class CscExample
+{
+	private int questionNo;
+	private String categary;
+	private String text;
+	
+	public void setQuestionNO(int que)
+	{
+	}
+	
+	public void setCategary(String cate)
+	{
+	}
+	
+	public void setText(String text)
+	{
+	}
+	
+	public int getQuestionNO()
+	{
+	}
+	
+	public String getCategary()
+	{ 
+	}
+	
+	public String getText()
+	{
+	}
+	
+	public void addQuestion(CscExample que)
+	{
+		
+	}
+	
+	public void removeQuestion(int quId)
+	{
+		
+	}
+	
+	public void editQuestion(int quId)
+	{
+		
+	}
+}

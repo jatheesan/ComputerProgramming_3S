61607786357c8470c48e0cd2f758f4c9a31c85e6
jatheesan <you@example.com>
2020-09-18 05:01:54 +0530

    modify the file on csc branch

--- CscExample.java
+++ CscExample.java
@@ -6,26 +6,32 @@ public class CscExample
 	
 	public void setQuestionNO(int que)
 	{
+		this.questionNo = que;
 	}
 	
 	public void setCategary(String cate)
 	{
+		this.categary = cate;
 	}
 	
 	public void setText(String text)
 	{
+		this.text = text;
 	}
 	
 	public int getQuestionNO()
 	{
+		return questionNo;
 	}
 	
 	public String getCategary()
 	{ 
+		return categary;
 	}
 	
 	public String getText()
 	{
+		return text;
 	}
 	
 	public void addQuestion(CscExample que)
